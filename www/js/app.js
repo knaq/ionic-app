283fd38bf8b0fef5a0a397411b31a26a5983b9dc
Shijir Tsogoo <shijir99@gmail.com>
2016-04-22 16:13:38 -0700

    changed the starter app to knaq app

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
-// Ionic Starter App
+// Ionic knaq App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'knaq' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'knaq.services' is found in services.js
+// 'knaq.controllers' is found in controllers.js
+angular.module('knaq', ['ionic', 'knaq.controllers', 'knaq.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('knaq.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
