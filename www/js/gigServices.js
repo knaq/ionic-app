ad9783a30dd1c2f2c683a5b3c0a6db87433d00ad
Michael Dang <michael.w.dang@gmail.com>
2016-05-11 16:42:15 -0700

    Modified gig service to use promises to make sure data is loaded before attempting to return data

--- www/js/gigServices.js
+++ www/js/gigServices.js
@@ -1,32 +1,24 @@
 angular.module('gig.services', ["firebase"])
-.service('GigFirebaseConnection', function($firebaseArray) {
-  this.firebaseReference = new Firebase("https://knaq.firebaseio.com/gigs");
-  this.gigs = $firebaseArray(this.firebaseReference);
-  console.log(this.gigs);
-  
-  this.getAll = function() {
-    return this.gigs;
-  };
-  
-  this.get = function(key) {
-    return this.gigs.$getRecord(key);
-  };
-  
-  this.remove = function(record) {
-    return this.gigs.$remove(record);
-  };
-  
-  this.add = function(title, pay, location, description, userId) {
-     this.gigs.$add({
-       title: title,
-       pay: pay,
-       location: location,
-       description: description,
-       userId: userId
-     });
-  };
-  
-  this.get = function() {
-    this.gigs
-  };
-});
+  .service('GigFirebaseConnection', function ($firebaseArray, $firebaseObject) {
+    this.firebaseReference = new Firebase("https://knaq.firebaseio.com/gigs");
+
+    this.getAll = function () {
+      var gigs = $firebaseArray(this.firebaseReference);
+      return gigs.$loaded();
+    };
+
+    this.get = function (key) {
+      return $firebaseObject(this.firebaseReference.child(key)).$loaded();
+    }
+
+    this.add = function (title, pay, location, description, userId) {
+      var gigs = $firebaseArray(this.firebaseReference);
+      gigs.$add({
+        title: title,
+        pay: pay,
+        location: location,
+        description: description,
+        userId: userId
+      });
+    };
+  });
