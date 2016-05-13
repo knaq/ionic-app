4534ea6e9eef80bfe4b1846c9a1cb18da30a8a77
Shijir Tsogoo <shijir99@gmail.com>
2016-05-13 15:39:50 -0700

    username is added in every gig object

--- www/js/gigControllers.js
+++ www/js/gigControllers.js
@@ -8,37 +8,50 @@ angular.module('gig.controllers', ['gig.services', 'knaq.services'])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
   this.gigLoadPromise = GigFirebaseConnection.getAll();
   this.gigs = null;
 
   this.gigLoadPromise.then(function(result) {
+
     this.gigs = result;
-    
+    this.gigs = this.gigs.map(function(gig) {
+
+      Data.getUser(gig.userId).then(function(userData) {
+        console.log(userData);
+        gig['username'] = userData.username;
+      })
+
+      return gig;
+      
+    })
+
+
   }.bind(this), function(error) {
-    alert("Couldn't load gig: " + error);  
+    alert("Couldn't load gig: " + error);
   })
-  
+
   this.getUserNameFromId = function(userId) {
     var posterLoadPromise = Data.getUser(user);
     posterLoadPromise.then(function() {
-      
+
     })
   }
-  
+
   this.createNewGig = function() {
     $state.go('tab.gig-new');
   }
+
 })
+
 
 .controller('GigDetailCtrl', function(GigFirebaseConnection, $stateParams, Session) {
   this.gigLoadPromise = GigFirebaseConnection.get($stateParams.gigId);
   this.gig = null;
-  
+
   this.applyBtnStates = [{
     label: "No function available",
     action: "doNothing()",
-    style: "button button-block button-light"     
+    style: "button button-block button-light"
   }, {
     label: "Apply",
     action: "apply()",
@@ -48,39 +61,38 @@ angular.module('gig.controllers', ['gig.services', 'knaq.services'])
     action: "unapply()",
     style: "button button-block button-assertive"
   }];
-  
-  
+
+
   this.checkApplyState = function() {
     if (this.gig == undefined || this.gig.applicants == undefined) {
       this.applyState = 0;
-    }
-    else if (this.gig.applicants.indexOf(Session.getUser()) == -1) {
+    } else if (this.gig.applicants.indexOf(Session.getUser()) == -1) {
       this.applyState = 1;
     } else {
       this.applyState = 2;
     }
   }
-  
+
   this.apply = function() {
     var userId = Session.getUser();
     if (this.gig.applicants == undefined || this.gig.applicants == null) {
-      this.gig.applicants = {userId};
+      this.gig.applicants = [userId];
       this.gig.$save();
     } else {
       this.gig.applicants.push(userId);
     }
   }
-  
+
   this.unapply = function() {
     var userId = Session.getUser();
-    var index = this.gig.applicants.indexOf(userId); 
-    while(index != -1) {
+    var index = this.gig.applicants.indexOf(userId);
+    while (index != -1) {
       this.gig.applicants.splice(index, 1);
       index = this.gig.applicants.indexOf(userId);
     }
     this.gig.$save();
   }
-  
+
   this.gigLoadPromise.then(function(result) {
     this.gig = result;
     console.log(this.gig);
@@ -90,10 +102,10 @@ angular.module('gig.controllers', ['gig.services', 'knaq.services'])
 })
 
 .controller('NewGigCtrl', function($ionicHistory, GigFirebaseConnection, Session) {
- 
-  /*Todo: Connect  userId to firebase authentication data*/ 
+
+  /*Todo: Connect  userId to firebase authentication data*/
   this.postGig = function() {
     GigFirebaseConnection.add(this.title, this.pay, this.location, this.description, Session.getUser());
-    $ionicHistory.goBack();  
+    $ionicHistory.goBack();
   }
 });
