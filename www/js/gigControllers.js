6cf7895eed12867579ee62aea45674285dc5c4fd
Michael Dang <michael.w.dang@gmail.com>
2016-05-12 23:07:14 -0700

    Defined potential apply button states for gig details page

--- www/js/gigControllers.js
+++ www/js/gigControllers.js
@@ -1,6 +1,6 @@
 angular.module('gig.controllers', ['gig.services', 'knaq.services'])
 
-.controller('GigsCtrl', function(GigFirebaseConnection, $state, $stateParams) {
+.controller('GigsCtrl', function(GigFirebaseConnection, $state, $stateParams, Data) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -19,6 +19,13 @@ angular.module('gig.controllers', ['gig.services', 'knaq.services'])
     alert("Couldn't load gig: " + error);  
   })
   
+  this.getUserNameFromId = function(userId) {
+    var posterLoadPromise = Data.getUser(user);
+    posterLoadPromise.then(function() {
+      
+    })
+  }
+  
   this.createNewGig = function() {
     $state.go('tab.gig-new');
   }
@@ -27,13 +34,31 @@ angular.module('gig.controllers', ['gig.services', 'knaq.services'])
 .controller('GigDetailCtrl', function(GigFirebaseConnection, $stateParams, Session) {
   this.gigLoadPromise = GigFirebaseConnection.get($stateParams.gigId);
   this.gig = null;
+  this.applyState = checkApplyState();
   
-  this.applyState = function() {
-    if (this.gig == undefined || this.gig.applicants == undefined) return -1;
-    if (this.gig.applicants.indexOf(Session.getUser()) == 0) {
-      return 0;
+  this.applyBtnStates = [{
+    label: "No function available",
+    action: "doNothing()",
+    style: "button button-block button-light"     
+  }, {
+    label: "Apply",
+    action: "apply()",
+    style: "button button-block button-positive"
+  }, {
+    label: "Unapply",
+    action: "unapply()",
+    style: "button button-block button-assertive"
+  }];
+  
+  
+  this.checkApplyState = function() {
+    if (this.gig == undefined || this.gig.applicants == undefined) {
+      this.applyState = 0;
+    }
+    if (this.gig.applicants.indexOf(Session.getUser()) == -1) {
+      this.applyState = 1;
     } else {
-      return 1;
+      this.applyState = 2;
     }
   }
   
