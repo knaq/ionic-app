503df2a8f069f54b5b09a5748cc12cbc0fc2028a
Michael Dang <michael.w.dang@gmail.com>
2016-05-18 16:19:47 -0700

    Replaced camera controller with image picker controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,23 @@
 angular.module('knaq.controllers', ['ionic', 'ngCordova'])
 
-.controller('ProfileCtrl', function($scope, $state, $firebaseAuth, Session, Data) {
+  .controller('ProfileCtrl', function ($scope, $state, $firebaseAuth, Session, Data) {
 
     $scope.allUsers = null;
     $scope.signedInUser = null;
 
-    Data.getAllUsers().then(function(data) {
+    Data.getAllUsers().then(function (data) {
       $scope.allUsers = data
     })
 
-    Data.getUser(Session.getUser()).then(function(data) {
+    Data.getUser(Session.getUser()).then(function (data) {
       $scope.signedInUser = data
     });
 
   })
-  .controller('SignUpCtrl', function($scope, $firebaseObject, $state) {
+  .controller('SignUpCtrl', function ($scope, $firebaseObject, $state) {
 
     $scope.signup = {};
-    $scope.reset = function() {
+    $scope.reset = function () {
 
       $scope.signup.email = "";
       $scope.signup.password = "";
@@ -25,7 +25,7 @@ angular.module('knaq.controllers', ['ionic', 'ngCordova'])
       $scope.signup.username = "";
 
     }
-    $scope.signUp = function() {
+    $scope.signUp = function () {
 
       var ref = new Firebase("https://knaq.firebaseio.com");
       var refToUsers = ref.child('users');
@@ -33,7 +33,7 @@ angular.module('knaq.controllers', ['ionic', 'ngCordova'])
       ref.createUser({
         email: $scope.signup.email,
         password: $scope.signup.password
-      }, function(error, userData) {
+      }, function (error, userData) {
         if (error) {
           console.log("Error creating user:", error);
         } else {
@@ -54,90 +54,87 @@ angular.module('knaq.controllers', ['ionic', 'ngCordova'])
     }
   })
 
-.controller('SignInCtrl', function($scope, $state, Session, Data) {
+  .controller('SignInCtrl', function ($scope, $state, Session, Data) {
 
-  var tmpUser = {};
+    var tmpUser = {};
 
-  $scope.signin = {};
+    $scope.signin = {};
 
-  $scope.reset = function() {
+    $scope.reset = function () {
 
-    $scope.signin.email = "";
-    $scope.signin.password = "";
+      $scope.signin.email = "";
+      $scope.signin.password = "";
 
-  }
-  $scope.signIn = function() {
+    }
+    $scope.signIn = function () {
 
-    var ref = new Firebase("https://knaq.firebaseio.com");
-
-
-    ref.authWithPassword({
-      email: $scope.signin.email,
-      password: $scope.signin.password
-    }, function(error, authData) {
-      if (error) {
-        console.log("Login Failed!", error);
-      } else {
-        //console.log("Authenticated successfully with payload:", authData);
-
-        //Passing authenticated users id to Auth Service
-        Session.setUser(authData.uid);
-
-        Data.setUserOnline(authData.uid).then(function(data) {
-          //console.log(data);
-          console.log("User with the following id" + authData.uid + "is successfully authenticated!");
-        });
-
-        $state.go('tab.profile');
-
-        $scope.signin.email = "";
-        $scope.signin.password = "";
-
-      }
-    });
-
-  }
-  $scope.signup = function() {
-
-    $state.go('signup');
-
-  }
+      var ref = new Firebase("https://knaq.firebaseio.com");
 
 
-})
+      ref.authWithPassword({
+        email: $scope.signin.email,
+        password: $scope.signin.password
+      }, function (error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          //console.log("Authenticated successfully with payload:", authData);
 
-.controller("CameraController", function($scope, $cordovaCamera) {
-  
-    $scope.takePicture = function() {
-        var options = { 
-            quality : 75, 
-            destinationType : Camera.DestinationType.DATA_URL, 
-            sourceType : Camera.PictureSourceType.CAMERA, 
-            allowEdit : true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
- 
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-            $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-            alert("Failed to take picture");
+          //Passing authenticated users id to Auth Service
+          Session.setUser(authData.uid);
+
+          Data.setUserOnline(authData.uid).then(function (data) {
+            //console.log(data);
+            console.log("User with the following id" + authData.uid + "is successfully authenticated!");
+          });
+
+          $state.go('tab.profile');
+
+          $scope.signin.email = "";
+          $scope.signin.password = "";
+
+        }
+      });
+
+    }
+    $scope.signup = function () {
+
+      $state.go('signup');
+
+    }
+
+
+  })
+
+  .controller('ImagePickerCtrl', function ($scope, $cordovaImagePicker) {
+    $scope.getPicture = function () {
+      var options = {
+        maximumImagesCount: 1,
+        width: 800,
+        height: 800,
+        quality: 80
+      };
+
+      $cordovaImagePicker.getPictures(options)
+        .then(function (results) {
+          $scope.imageURI = results[0];
+        }, function (error) {
+          alert("Error getting photo");
         });
     }
-})
+  })
 
 
-.controller('AccountCtrl', function($scope, $state, Data, Session) {
-  $scope.account = {};
-  $scope.account.signout = function(arguments) {
-    console.log("signing out")
-      Data.setUserOffline(Session.getUser()).then(function(data) {
+  .controller('AccountCtrl', function ($scope, $state, Data, Session) {
+    $scope.account = {};
+    $scope.account.signout = function (arguments) {
+      console.log("signing out")
+      Data.setUserOffline(Session.getUser()).then(function (data) {
         console.log("User is offline now!");
       });
       Session.logout();
       $state.go('signin');
     }
-});
+  });
+
+
