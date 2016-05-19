e2ab0b72243b6887f8f9b9975a6d7f7c0f899030
Michael Dang <michael.w.dang@gmail.com>
2016-05-19 13:50:35 -0700

    Fixed spacing issues

--- www/js/gigControllers.js
+++ www/js/gigControllers.js
@@ -1,112 +1,128 @@
-angular.module('gig.controllers', ['gig.services', 'knaq.services', 'knaq.controllers'])
+angular.module('gig.controllers', ['gig.services', 'knaq.services', 'knaq.controllers', 'base64.services', 'imageGallery.services'])
 
-.controller('GigsCtrl', function($scope, GigFirebaseConnection, $state, $stateParams, Data) {
+  .controller('GigsCtrl', function ($scope, GigFirebaseConnection, $state, $stateParams, Data) {
 
-  $scope.gigs = {}
+    $scope.gigs = {}
 
-  GigFirebaseConnection.getAll().then(function(result) {
+    GigFirebaseConnection.getAll().then(function (result) {
 
 
-    var addUsername = function() {
-      $scope.gigs.allGigList = $scope.gigs.retrievedGigList.map(function(gig) {
+      var addUsername = function () {
+        $scope.gigs.allGigList = $scope.gigs.retrievedGigList.map(function (gig) {
 
-        Data.getUser(gig.userId).then(function(userData) {
-          gig['username'] = userData.username;
+          Data.getUser(gig.userId).then(function (userData) {
+            gig['username'] = userData.username;
+          })
+
+          return gig;
+
         })
+      }
 
-        return gig;
+      $scope.gigs.retrievedGigList = result;
+      addUsername()
 
+      result.$watch(function () {
+        addUsername();
       })
-    }
 
-    $scope.gigs.retrievedGigList = result;
-    addUsername()
 
-    result.$watch(function() {
-      addUsername();
+
     })
 
+  })
+
+
+  .controller('GigDetailCtrl', function ($scope, GigFirebaseConnection, $stateParams, Session) {
+
+    $scope.gigDetail = {}
+
+    var gigLoadPromise = GigFirebaseConnection.get($stateParams.gigId);
+
+    gigLoadPromise.then(function (result) {
+      $scope.gigDetail.gig = result;
+      console.log("Gig detail:")
+      console.log($scope.gigDetail.gig);
+    })
+
+    $scope.gigDetail.applyBtnStates = [{
+      label: "No function available",
+      action: "doNothing()",
+      style: "button button-block button-light"
+    }, {
+        label: "Apply",
+        action: "apply()",
+        style: "button button-block button-positive"
+      }, {
+        label: "Unapply",
+        action: "unapply()",
+        style: "button button-block button-assertive"
+      }];
+
+
+    $scope.gigDetail.checkApplyState = function () {
+      if ($scope.gigDetail.gig == undefined || $scope.gigDetail.gig.applicants == undefined) {
+        $scope.gigDetail.applyState = 0;
+      } else if ($scope.gigDetail.gig.applicants.indexOf(Session.getUser()) == -1) {
+        $scope.gigDetail.applyState = 1;
+      } else {
+        $scope.gigDetail.applyState = 2;
+      }
+    }
+
+    $scope.gigDetail.apply = function () {
+      var userId = Session.getUser();
+
+      if ($scope.gigDetail.gig.applicants == undefined || $scope.gigDetail.gig.applicants == null) {
+        $scope.gigDetail.gig.applicants = [userId];
+        $scope.gigDetail.gig.$save();
+      } else {
+        $scope.gigDetail.gig.applicants.push(userId);
+        $scope.gigDetail.gig.$save();
+      }
+    }
+
+    $scope.gigDetail.unapply = function () {
+      var userId = Session.getUser();
+      var index = $scope.gigDetail.gig.applicants.indexOf(userId);
+      while (index != -1) {
+        $scope.gigDetail.gig.applicants.splice(index, 1);
+        index = $scope.gigDetail.gig.applicants.indexOf(userId);
+      }
+      $scope.gigDetail.gig.$save();
+    }
 
 
   })
 
-})
+  .controller('NewGigCtrl', function ($scope, $state, $ionicHistory, GigFirebaseConnection, Session, Base64, ImageGallery) {
 
+    /*Todo: Connect  userId to firebase authentication data*/
+    $scope.newGig = {}
+    $scope.imageURI = null;
 
-.controller('GigDetailCtrl', function($scope, GigFirebaseConnection, $stateParams, Session) {
-
-  $scope.gigDetail = {}
-
-  var gigLoadPromise = GigFirebaseConnection.get($stateParams.gigId);
-
-  gigLoadPromise.then(function(result) {
-    $scope.gigDetail.gig = result;
-    console.log("Gig detail:")
-    console.log($scope.gigDetail.gig);
-  })
-
-  $scope.gigDetail.applyBtnStates = [{
-    label: "No function available",
-    action: "doNothing()",
-    style: "button button-block button-light"
-  }, {
-    label: "Apply",
-    action: "apply()",
-    style: "button button-block button-positive"
-  }, {
-    label: "Unapply",
-    action: "unapply()",
-    style: "button button-block button-assertive"
-  }];
-
-
-  $scope.gigDetail.checkApplyState = function() {
-    if ($scope.gigDetail.gig == undefined || $scope.gigDetail.gig.applicants == undefined) {
-      $scope.gigDetail.applyState = 0;
-    } else if ($scope.gigDetail.gig.applicants.indexOf(Session.getUser()) == -1) {
-      $scope.gigDetail.applyState = 1;
-    } else {
-      $scope.gigDetail.applyState = 2;
+    $scope.getPicture = function () {
+      ImageGallery.getPicture().then(function (results) {
+        $scope.imageURI = results[0];
+      });
     }
-  }
-
-  $scope.gigDetail.apply = function() {
-    var userId = Session.getUser();
-
-    if ($scope.gigDetail.gig.applicants == undefined || $scope.gigDetail.gig.applicants == null) {
-      $scope.gigDetail.gig.applicants = [userId];
-      $scope.gigDetail.gig.$save();
-    } else {
-      $scope.gigDetail.gig.applicants.push(userId);
-      $scope.gigDetail.gig.$save();
+    
+    $scope.getImageUri = function () {
+      console.log("running getImageUri");
+      alert($scope.imageURI);
     }
-  }
 
-  $scope.gigDetail.unapply = function() {
-    var userId = Session.getUser();
-    var index = $scope.gigDetail.gig.applicants.indexOf(userId);
-    while (index != -1) {
-      $scope.gigDetail.gig.applicants.splice(index, 1);
-      index = $scope.gigDetail.gig.applicants.indexOf(userId);
+
+    $scope.newGig.postGig = function (imageUrl) {
+      alert("imageurl: " + imageUrl);
+      var imageData = Base64.getDataUrlFromUrl(imageUrl, function () {
+        //GigFirebaseConnection.add($scope.newGig.title, $scope.newGig.pay, $scope.newGig.location, $scope.newGig.description, imageURI, Session.getUser());
+      }.bind($scope));
+      $scope.newGig.title = ""
+      $scope.newGig.pay = ""
+      $scope.newGig.location = ""
+      $scope.newGig.description = ""
+      $scope.newGig.imageURI = ""
+      $state.go('tab.gigs');
     }
-    $scope.gigDetail.gig.$save();
-  }
-
-
-})
-
-.controller('NewGigCtrl', function($scope, $state, $ionicHistory, GigFirebaseConnection, Session) {
-
-  /*Todo: Connect  userId to firebase authentication data*/
-  $scope.newGig = {}
-
-
-  $scope.newGig.postGig = function() {
-    GigFirebaseConnection.add($scope.newGig.title, $scope.newGig.pay, $scope.newGig.location, $scope.newGig.description, Session.getUser());
-    $scope.newGig.title = ""
-    $scope.newGig.pay = ""
-    $scope.newGig.location = ""
-    $scope.newGig.description = ""
-    $state.go('tab.gigs');
-  }
-});
+  });
