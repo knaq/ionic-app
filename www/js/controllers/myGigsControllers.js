adb5b4c6550c982c8c8c78f47eae85d54c35b222
Shijir Tsogoo <shijir99@gmail.com>
2016-05-19 13:05:37 -0700

    updated my gigs controller

--- www/js/controllers/myGigsControllers.js
+++ www/js/controllers/myGigsControllers.js
@@ -1,6 +1,12 @@
 angular.module('knaq.myGigsControllers', [])
-.controller('MyGigsCtrl', function($scope, $state, Data, Auth) {
-    $scope.myGigs = {};
-    $scope.myGigs.pageTitle = "My gigs go here"
-    
-});
+	.controller('MyGigsCtrl', function($scope, $state, Data, Auth) {
+		$scope.myGigs = {};
+		$scope.myGigs.activeTab = 1;
+
+		$scope.myGigs.tabClick = function(tab) {
+			$scope.myGigs.activeTab = tab;
+			console.log($scope.myGigs.activeTab)
+		}
+		$scope.myGigs.pageTitle = "My gigs go here"
+
+	});
