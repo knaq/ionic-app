da71f0dc9b5637879027f570131b35633fa6768c
Shijir Tsogoo <shijir99@gmail.com>
2016-05-20 20:55:44 -0700

    implemented loader in my gigs page

--- www/js/controllers/myGigsControllers.js
+++ www/js/controllers/myGigsControllers.js
@@ -2,6 +2,7 @@ angular.module('knaq.myGigsControllers', [])
 	.controller('MyGigsCtrl', function($scope, $state, Data, Auth, MyGigsServices) {
 		$scope.myGigs = {};
 		$scope.myGigs.tabSelection = 'tab-inprogress';
+		$scope.myGigs.loadingData = true;
 		MyGigsServices.getInProgress().then(function(gigsInProgress) {
 			$scope.myGigs.gigsInProgress = gigsInProgress
 		}, function(error) {
@@ -9,28 +10,35 @@ angular.module('knaq.myGigsControllers', [])
 		})
 
 		$scope.myGigs.tabClick = function(selection) {
+			console.log("tab change")
 
 			$scope.myGigs.tabSelection = selection;
 
 			switch ($scope.myGigs.tabSelection) {
 				case "tab-myposts":
 					MyGigsServices.getPosts().then(function(myPosts) {
+						$scope.myGigs.loadingData = false;
 						$scope.myGigs.myPosts = myPosts
 					}, function(error) {
+						$scope.myGigs.loadingData = false;
 						console.error(error)
 					})
 					break;
 				case "tab-applied":
 					MyGigsServices.getApplied().then(function(myAppliedGigs) {
+						$scope.myGigs.loadingData = false;
 						$scope.myGigs.myAppliedGigs = myAppliedGigs
 					}, function(error) {
+						$scope.myGigs.loadingData = false;
 						console.error(error)
 					})
 					break;
 				default:
 					MyGigsServices.getInProgress().then(function(gigsInProgress) {
+						$scope.myGigs.loadingData = false;
 						$scope.myGigs.gigsInProgress = gigsInProgress
 					}, function(error) {
+						$scope.myGigs.loadingData = false;
 						console.error(error)
 					})
 			}
