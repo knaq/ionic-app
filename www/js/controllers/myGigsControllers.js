d506f4f5468dfe42cd5dc0281edab8d6df43ad6f
Shijir Tsogoo <shijir99@gmail.com>
2016-05-20 17:04:44 -0700

    fixed html in mygigs

--- www/js/controllers/myGigsControllers.js
+++ www/js/controllers/myGigsControllers.js
@@ -1,35 +1,39 @@
 angular.module('knaq.myGigsControllers', [])
 	.controller('MyGigsCtrl', function($scope, $state, Data, Auth, MyGigsServices) {
 		$scope.myGigs = {};
-		$scope.myGigs.activeTab = 1;
+		$scope.myGigs.tabSelection = 'tab-inprogress';
+		MyGigsServices.getInProgress().then(function(gigsInProgress) {
+			$scope.myGigs.gigsInProgress = gigsInProgress
+		}, function(error) {
+			console.error(error)
+		})
 
 		$scope.myGigs.tabClick = function(selection) {
 
 			$scope.myGigs.tabSelection = selection;
-			
-			switch($scope.myGigs.tabSelection){
+
+			switch ($scope.myGigs.tabSelection) {
 				case "tab-myposts":
-					MyGigsServices.getPosts().then(function (myPosts) {
-						$scope.myGigs.myPosts = myPosts 
-					}, function (error) {
+					MyGigsServices.getPosts().then(function(myPosts) {
+						$scope.myGigs.myPosts = myPosts
+					}, function(error) {
 						console.error(error)
 					})
 					break;
 				case "tab-applied":
-					MyGigsServices.getApplied().then(function (myAppliedGigs) {
+					MyGigsServices.getApplied().then(function(myAppliedGigs) {
 						$scope.myGigs.myAppliedGigs = myAppliedGigs
-					},function (error) {
+					}, function(error) {
 						console.error(error)
 					})
 					break;
 				default:
-					MyGigsServices.getInProgress().then(function (gigsInProgress) {
+					MyGigsServices.getInProgress().then(function(gigsInProgress) {
 						$scope.myGigs.gigsInProgress = gigsInProgress
-					},function (error) {
+					}, function(error) {
 						console.error(error)
 					})
 			}
 		}
-		$scope.myGigs.pageTitle = "My gigs go here"
 
 	});
